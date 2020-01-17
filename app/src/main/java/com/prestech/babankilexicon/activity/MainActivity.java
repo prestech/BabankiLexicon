c4f60bb290d773db721c9118c91b5742a08fdfbc
presley Muwan <muwanpresley@gmail.com>
2020-01-16 20:43:50 -0500

    added intro to home page

--- app/src/main/java/com/prestech/babankilexicon/activity/MainActivity.java
+++ app/src/main/java/com/prestech/babankilexicon/activity/MainActivity.java
@@ -56,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
 
         fragmentManager = getSupportFragmentManager();
 
+        openFragment(new HomeFragment());
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
